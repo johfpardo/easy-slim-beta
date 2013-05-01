c97e3b309a2817c024711060dbbb7681ed6e4391
ajalvareze <ajalvareze@unal.edu.co>
2013-04-30 19:21:07 -0500

    Autor: Antonio Alvarez Change: replace hard coded strings with string variables to test mercurial en eclipse

--- src/com/example/easy_slim_beta/AppCaloriasActivity.java
+++ src/com/example/easy_slim_beta/AppCaloriasActivity.java
@@ -1,12 +1,8 @@
 package com.example.easy_slim_beta;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.view.View;
-import android.view.View.OnClickListener;
 
 
 public class AppCaloriasActivity extends Activity {
