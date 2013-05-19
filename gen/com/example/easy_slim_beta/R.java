2cfd2d497452b3587bfa5bfbdef5966e53a43945
anmguerreroam <anmguerreroam@unal.edu.co>
2013-05-19 00:29:29 -0500

    Commit by: Andres Guerrero Changes: Porciones preview

--- gen/com/example/easy_slim_beta/R.java
+++ gen/com/example/easy_slim_beta/R.java
@@ -8,6 +8,15 @@
 package com.example.easy_slim_beta;
 
 public final class R {
+    public static final class anim {
+        public static final int custombutton=0x7f040000;
+        public static final int custombutton1=0x7f040001;
+        public static final int custombuttondairy=0x7f040002;
+        public static final int custombuttonfruit=0x7f040003;
+        public static final int custombuttongrains=0x7f040004;
+        public static final int custombuttonprotein=0x7f040005;
+        public static final int custombuttonvegetables=0x7f040006;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,47 +26,64 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int fathuman=0x7f020001;
-        public static final int human=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int logo2=0x7f020004;
-        public static final int normalhuman=0x7f020005;
-        public static final int thinhuman=0x7f020006;
+        public static final int button1=0x7f020001;
+        public static final int button2=0x7f020002;
+        public static final int button3=0x7f020003;
+        public static final int button4=0x7f020004;
+        public static final int dairy=0x7f020005;
+        public static final int fathuman=0x7f020006;
+        public static final int fruits=0x7f020007;
+        public static final int grains=0x7f020008;
+        public static final int human=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int logo2=0x7f02000b;
+        public static final int normalhuman=0x7f02000c;
+        public static final int plate=0x7f02000d;
+        public static final int protein=0x7f02000e;
+        public static final int thinhuman=0x7f02000f;
+        public static final int vegetables=0x7f020010;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001b;
-        public static final int button1=0x7f080007;
-        public static final int buttonCalorias=0x7f08000c;
-        public static final int buttonIMC=0x7f080008;
-        public static final int buttonModif=0x7f08000a;
-        public static final int buttonPorcion=0x7f08000b;
-        public static final int buttonRecomend=0x7f080009;
-        public static final int editTextHeight=0x7f080013;
-        public static final int editTextName=0x7f08000f;
-        public static final int editTextWeigth=0x7f080015;
-        public static final int imageInfo=0x7f080005;
-        public static final int imageView1=0x7f08000d;
-        public static final int menu_settings=0x7f08001a;
-        public static final int nextButton=0x7f080003;
-        public static final int pickDate=0x7f080011;
-        public static final int prevButton=0x7f080002;
-        public static final int radioGroupSex=0x7f080017;
-        public static final int radioMan=0x7f080018;
-        public static final int radioWoman=0x7f080019;
-        public static final int recomendationImage=0x7f080000;
-        public static final int recomendationText=0x7f080001;
-        public static final int textInfo=0x7f080006;
-        public static final int textView1=0x7f080004;
-        public static final int textViewBirth=0x7f080010;
-        public static final int textViewHeight=0x7f080012;
-        public static final int textViewName=0x7f08000e;
-        public static final int textViewSex=0x7f080016;
-        public static final int textViewWeight=0x7f080014;
+        public static final int action_settings=0x7f090022;
+        public static final int button1=0x7f090007;
+        public static final int button2=0x7f090013;
+        public static final int button3=0x7f090011;
+        public static final int button4=0x7f090014;
+        public static final int button5=0x7f09000f;
+        public static final int buttonCalorias=0x7f09000c;
+        public static final int buttonIMC=0x7f090008;
+        public static final int buttonModif=0x7f09000a;
+        public static final int buttonPorcion=0x7f09000b;
+        public static final int buttonRecomend=0x7f090009;
+        public static final int editTextHeight=0x7f09001a;
+        public static final int editTextName=0x7f090016;
+        public static final int editTextWeigth=0x7f09001c;
+        public static final int imageInfo=0x7f090005;
+        public static final int imageView1=0x7f09000d;
+        public static final int menu_settings=0x7f090021;
+        public static final int nextButton=0x7f090003;
+        public static final int pickDate=0x7f090018;
+        public static final int prevButton=0x7f090002;
+        public static final int radioGroupSex=0x7f09001e;
+        public static final int radioMan=0x7f09001f;
+        public static final int radioWoman=0x7f090020;
+        public static final int recomendationImage=0x7f090000;
+        public static final int recomendationText=0x7f090001;
+        public static final int tableRow1=0x7f090010;
+        public static final int tableRow2=0x7f090012;
+        public static final int tableRow3=0x7f09000e;
+        public static final int textInfo=0x7f090006;
+        public static final int textView1=0x7f090004;
+        public static final int textViewBirth=0x7f090017;
+        public static final int textViewHeight=0x7f090019;
+        public static final int textViewName=0x7f090015;
+        public static final int textViewSex=0x7f09001d;
+        public static final int textViewWeight=0x7f09001b;
     }
     public static final class layout {
         public static final int activity_advice=0x7f030000;
@@ -68,51 +94,51 @@ public final class R {
         public static final int register=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_menu=0x7f070000;
-        public static final int advice=0x7f070001;
-        public static final int main=0x7f070002;
+        public static final int activity_menu=0x7f080000;
+        public static final int advice=0x7f080001;
+        public static final int main=0x7f080002;
     }
     public static final class string {
-        public static final int Calorias=0x7f050003;
-        public static final int Menu=0x7f050004;
-        public static final int Next=0x7f050026;
-        public static final int Previous=0x7f050027;
-        public static final int Recomendaciones=0x7f050002;
-        public static final int accept=0x7f050009;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int askBirthDay=0x7f050006;
-        public static final int askHeight=0x7f050007;
-        public static final int askName=0x7f050005;
-        public static final int askSex=0x7f050018;
-        public static final int askWeight=0x7f050008;
-        public static final int calorias=0x7f05000b;
-        public static final int day=0x7f050020;
-        public static final int dietsB=0x7f050010;
-        public static final int foodB=0x7f050011;
-        public static final int height=0x7f050021;
-        public static final int hello_world=0x7f050025;
-        public static final int info_condition=0x7f050014;
-        public static final int info_fat=0x7f050017;
-        public static final int info_normal=0x7f050016;
-        public static final int info_thin=0x7f050015;
-        public static final int info_title=0x7f050013;
-        public static final int man=0x7f050019;
-        public static final int menu_settings=0x7f05000e;
-        public static final int month=0x7f05001f;
-        public static final int name=0x7f05001d;
-        public static final int none=0x7f05000c;
-        public static final int pickDate=0x7f05001b;
-        public static final int profile_file=0x7f05000a;
-        public static final int sex=0x7f050023;
-        public static final int title_activity_advice=0x7f050024;
-        public static final int title_activity_menu=0x7f05000f;
-        public static final int user_profile=0x7f05001c;
-        public static final int weight=0x7f050022;
-        public static final int weightB=0x7f050012;
-        public static final int welcome=0x7f05000d;
-        public static final int woman=0x7f05001a;
-        public static final int year=0x7f05001e;
+        public static final int Calorias=0x7f060003;
+        public static final int Menu=0x7f060004;
+        public static final int Next=0x7f060026;
+        public static final int Previous=0x7f060027;
+        public static final int Recomendaciones=0x7f060002;
+        public static final int accept=0x7f060009;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int askBirthDay=0x7f060006;
+        public static final int askHeight=0x7f060007;
+        public static final int askName=0x7f060005;
+        public static final int askSex=0x7f060018;
+        public static final int askWeight=0x7f060008;
+        public static final int calorias=0x7f06000b;
+        public static final int day=0x7f060020;
+        public static final int dietsB=0x7f060010;
+        public static final int foodB=0x7f060011;
+        public static final int height=0x7f060021;
+        public static final int hello_world=0x7f060025;
+        public static final int info_condition=0x7f060014;
+        public static final int info_fat=0x7f060017;
+        public static final int info_normal=0x7f060016;
+        public static final int info_thin=0x7f060015;
+        public static final int info_title=0x7f060013;
+        public static final int man=0x7f060019;
+        public static final int menu_settings=0x7f06000e;
+        public static final int month=0x7f06001f;
+        public static final int name=0x7f06001d;
+        public static final int none=0x7f06000c;
+        public static final int pickDate=0x7f06001b;
+        public static final int profile_file=0x7f06000a;
+        public static final int sex=0x7f060023;
+        public static final int title_activity_advice=0x7f060024;
+        public static final int title_activity_menu=0x7f06000f;
+        public static final int user_profile=0x7f06001c;
+        public static final int weight=0x7f060022;
+        public static final int weightB=0x7f060012;
+        public static final int welcome=0x7f06000d;
+        public static final int woman=0x7f06001a;
+        public static final int year=0x7f06001e;
     }
     public static final class style {
         /** 
@@ -136,10 +162,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
