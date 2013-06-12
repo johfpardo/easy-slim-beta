0badbfda0157a9f89d550eb3e37af239b719a99e
anmguerreroam <anmguerreroam@unal.edu.co>
2013-06-11 22:49:08 -0500

    commit by: Andres Guerrero changes: Add  landscape layouts and modified portions.xml and infoportion.xml files of landscape layout, implemented methods prev and next in InfoPortionActivity.java

--- src/com/example/easy_slim_beta/InfoPortionActivity.java
+++ src/com/example/easy_slim_beta/InfoPortionActivity.java
@@ -1,9 +1,5 @@
 package com.example.easy_slim_beta;
 
-import java.util.List;
-
-//import easy_slim_beta.entities.Portion;
-import easy_slim_beta.entities.Recomendation;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,60 +9,86 @@ import android.widget.TextView;
 
 public class InfoPortionActivity extends Activity{
 	
+	private int index;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.infoporcion);
+		
+		index = 0;
 
 		Intent i = getIntent();
 		String msg = i.getStringExtra("Type");
 		
-		setPortion(msg);
+		setPortion(msg, index);
 	}
 	
-	public void setPortion(String msg) {
+	public void setPortion(String msg, int index) {
 		TextView t = (TextView) findViewById(R.id.portionText);
 		ImageView ri = (ImageView) findViewById(R.id.portionImage);
 		
-		if(msg.contentEquals("Vegetables")){
+		if(msg.contentEquals("Vegetables") || index == 0){
+			setTitle("Hortalizas, Verduras Y Leguminosas Verdes");
 			t.setText(getString(R.string.Vegetables));
-			ri.setImageResource(R.drawable.vegetables);
+			ri.setImageResource(R.drawable.r_fruitsandvegetables);
 		}
 		
-		if(msg.contentEquals("Fruits")){
+		if(msg.contentEquals("Fruits") || index == 1){               
+			setTitle("Frutas");
 			t.setText(getString(R.string.Fruits));
-			ri.setImageResource(R.drawable.fruits);
+			ri.setImageResource(R.drawable.r_fruits);
 		}
 		
-		if(msg.contentEquals("Cereals")){
+		if(msg.contentEquals("Cereals") || index == 2){
+			setTitle("Cereales");
 			t.setText(getString(R.string.Cereals));
 			ri.setImageResource(R.drawable.grains);
 		}
 		
-		if(msg.contentEquals("Roots")){
+		if(msg.contentEquals("Roots") || index == 3){
+			setTitle("Raíces, Tubérculos Y Plátanos");
 			t.setText(getString(R.string.Roots));
 			ri.setImageResource(R.drawable.protein);
 		}
 		
-		if(msg.contentEquals("meategg")){
+		if(msg.contentEquals("meategg") || index == 4){
+			setTitle("Carnes, Huevos, Leguminosas");
 			t.setText(getString(R.string.Meat));
 			ri.setImageResource(R.drawable.protein);
 		}
 		
-		if(msg.contentEquals("Dairy")){
+		if(msg.contentEquals("Dairy") || index == 5){
+			setTitle("Lacteos");
 			t.setText(getString(R.string.Dairy));
 			ri.setImageResource(R.drawable.dairy);
 		}
 		
-		if(msg.contentEquals("Sugars")){
+		if(msg.contentEquals("Sugars") || index == 6){
+			setTitle("Azucares y Dulces");
 			t.setText(getString(R.string.Sugar));
 			ri.setImageResource(R.drawable.bpeach);
 		}
 		
-		if(msg.contentEquals("Fat")){
+		if(msg.contentEquals("Fat") || index == 7){
+			setTitle("Grasas");
 			t.setText(getString(R.string.Fat));
 			ri.setImageResource(R.drawable.fathuman);
 		}		
+	}
+	
+	public void next(View view) {
+		index++;
+		String msg = "Type";
+		if(index == 8) index=0;
+		setPortion(msg, index);
+	}
+	
+	public void prev(View view) {
+		index--;
+		String msg = "Type";
+		if(index ==-1) index=7;
+		setPortion(msg, index);
 	}
 }
