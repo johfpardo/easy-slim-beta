34721961ff1f7ca751e67f8062dd0d7aa8980ee0
snhernandezco <snhernandezco@unal.edu.co>
2013-06-17 12:15:15 -0500

    Commit by: Sneider Hernandez Changes: -navigation with gestures in recommendations view added -animations in change of recommendation

--- src/com/example/easy_slim_beta/AdviceActivity.java
+++ src/com/example/easy_slim_beta/AdviceActivity.java
@@ -4,33 +4,66 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.animation.AnimationUtils;
+import android.widget.*;
 import easy_slim_beta.dao.RecomendationDao;
 import easy_slim_beta.entities.Recomendation;
+import easy_slim_beta.swipe.SwipeGestureDetector;
+import easy_slim_beta.swipe.Swipeable;
 
-public class AdviceActivity extends Activity {
+public class AdviceActivity extends Activity  implements Swipeable {
+	// list of recommendation 
 	private List<Recomendation> recomendations;
-	private int index;
+	// index or current recommendation
+	private int index = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_advice);
+		
 		recomendations = new RecomendationDao(this).getRecomendationForBMI(MainMenuActivity.user.getImc());
-		index=0;
+		// show the first recommendation
 		setRecomendation();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.advice, menu);
-		return true;
+		// message for user's navigation
+		Toast.makeText(this, getString(R.string.navigate), Toast.LENGTH_LONG).show();
 	}
 	
+	// let the user navigate with gestures on screen
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		GestureDetector gestureDetector = new GestureDetector(new SwipeGestureDetector(this));
+		if (gestureDetector.onTouchEvent(event)) return true;
+		return super.onTouchEvent(event);
+	}
+	
+	// set the next recommendation
+		@Override
+		public void onLeftSwipe(View view) {
+			index = (index == 0) ? recomendations.size()-1 : index-1;
+			findViewById(R.id.advice_layout).setAnimation(
+					AnimationUtils.loadAnimation(this, R.anim.slide_in_right));
+			findViewById(R.id.advice_layout).setAnimation(
+					AnimationUtils.loadAnimation(this, R.anim.slide_out_right));
+			setRecomendation();
+		}
+		
+		// set the previous recommendation
+		@Override
+		public void onRightSwipe(View view) {
+			index = (index == recomendations.size()-1) ? 0 : index+1;
+			findViewById(R.id.advice_layout).setAnimation(
+					AnimationUtils.loadAnimation(this, R.anim.slide_in_left));
+			findViewById(R.id.advice_layout).setAnimation(
+					AnimationUtils.loadAnimation(this, R.anim.slide_out_left));
+			setRecomendation();
+		}
+		
+		// set the recommendation
 	public void setRecomendation() {
 		ImageView ri = (ImageView) findViewById(R.id.recomendationImage);
 		TextView rt = (TextView) findViewById(R.id.recomendationText);
@@ -39,21 +72,9 @@ public class AdviceActivity extends Activity {
 			ri.setImageResource(getResources().getIdentifier(
 				recomendations.get(index).getImageName(), "drawable", getPackageName()));
 			rt.setText(recomendations.get(index).getTextRecomendation());
-			nr.setText("Recomendacion "+(index+1)+" de "+recomendations.size());
+			nr.setText(getString(R.string.recommendation) + " " + (index+1) + " / " + recomendations.size());
 		} catch(Exception e) {
-			rt.setText("No se encontraron Recomendaciones para usted.");
+			rt.setText(getString(R.string.recommendationsNotFound));
 		}
-	}
-	
-	public void nextRecomendation(View view) {
-		index++;
-		if(index==recomendations.size()) index=0;
-		setRecomendation();
-	}
-	
-	public void prevRecomendation(View view) {
-		index--;
-		if(index==-1) index+=recomendations.size();
-		setRecomendation();
 	}
 }
---
+++ src/easy_slim_beta/swipe/Swipeable.java
@@ -0,0 +1,8 @@
+package easy_slim_beta.swipe;
+
+import android.view.View;
+
+public interface Swipeable {
+	void onLeftSwipe(View view);
+	void onRightSwipe(View view);
+}
